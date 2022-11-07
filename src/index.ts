371cc8e737fbb86f1ecdb1f988ffe58b020e1733
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-11-07 18:06:33 +0100

    Statically vs Dynamically Typed Languages

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log(Math.floor(10.12))
+// let num = 10;
+// num = "kamal";
+// console.log(num)
+
+// cheking err : Property 'repeat' does not exist on type 'number'
+let age = 40;
+if(age > 40){
+  console.log("yeep")
+}else{
+    console.log(age.repeat(3))
+}
+// Property 'repeat' does not exist on type 'void'
+let name = "kamal";
+if(name > 40){
+  console.log("yeep")
+}else{
+    console.log(name.repeat(3))
+}
