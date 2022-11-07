312dbb6045b4fd5ba86d83577bac36fa2df94f3a
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-11-07 18:14:45 +0100

    Type Annotations And Any Data Type

--- src/index.ts
+++ src/index.ts
@@ -16,3 +16,14 @@ if(name > 40){
 }else{
     console.log(name.repeat(3))
 }
+
+// Type Annotations And Any Data Type
+let myname: string = "kamal";
+let myage: number = 20;
+let love: boolean = true;
+
+ // Type 'number' is not assignable to type 'string'.
+ myname= 33;
+// solution for the err above
+ let Data: any = "hi there"
+ Data = 44;
