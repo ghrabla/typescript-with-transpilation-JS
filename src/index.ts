5aeff5ae1346de28255d19f2dad0edf4c3fc27aa
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-11-09 04:20:31 +0100

    Union And Intersection Types

--- src/index.ts
+++ src/index.ts
@@ -265,3 +265,43 @@ console.log(myImg.src);
 
 let data: any = 1000;
 console.log((data as string).repeat(3));
+
+/*
+  Data Types
+  - Union And Intersection Types
+  --- Union Type
+  ------ The | Symbol Is Used To Create The Union => "Or"
+
+  --- Intersection Type
+  ------ Is A Type That Combines Several Types Into One
+  ------ The & Symbol Is Used To Create An Intersection => "And"
+
+  --- If A Union Is An OR, Then An Intersection Is An AND.
+*/
+
+// let all: number | string = 100;
+
+type A = {
+    one: string,
+    two: number,
+    three: boolean
+  }
+  
+  type B = A & {
+    four: number
+  }
+  
+  type C = {
+    five: boolean
+  }
+  
+  type mix = A & C;
+  
+  function getActions1(btns: mix) {
+    console.log(`Hello ${btns.one}`);
+    console.log(`Hello ${btns.two}`);
+    console.log(`Hello ${btns.three}`);
+    console.log(`Hello ${btns.five}`);
+  }
+  
+  getActions1({ one: "String", two: 100, three: true, five: true });
