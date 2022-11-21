f51f5906845d884d2c8ed3453511ac9c492c0a36
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-11-21 15:24:16 +0100

    interface implement

--- src/index.ts
+++ src/index.ts
@@ -502,3 +502,32 @@ let u2 = new Body("ghrabla");
 let u3 = new Body("ghr");
 // console.log(Body.created);
 Body.getCount();
+
+/*
+  Class
+  - Implement Interface
+*/
+
+interface Settings {
+  theme: boolean;
+  font: string;
+  save(): void;
+}
+
+class User implements Settings {
+  constructor(public username: string, public theme: boolean, public font: string) {}
+  save(): void {
+    console.log(`Saved`);
+  }
+  update(): void {
+    console.log(`Updated`);
+  }
+}
+
+let userOne = new User("Elzero", true, "Open Sans");
+
+console.log(userOne.username);
+console.log(userOne.font);
+
+userOne.save();
+userOne.update();
